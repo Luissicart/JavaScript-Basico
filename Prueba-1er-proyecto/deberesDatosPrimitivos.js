98ff3dbaf341b1e8e9f4c0972a70f86f636e95ab
Luissicart <109523221+Luissicart@users.noreply.github.com>
2022-10-28 14:23:45 +0200

    Add files via upload

---
+++ Prueba-1er-proyecto/deberesDatosPrimitivos.js
@@ -0,0 +1,12 @@
+nombre = "Luis Miguel";
+edad = 28;
+developer = true;
+birthday = new Date (1994, 10, 05);
+libroFavorito={
+    titulo: "Chainsaw Man",
+    autor: "Tatsuki Fujimoto",
+    fecha: new Date (2018, 11, 03),
+    enlace: "es.wikipedia.org/wiki/Chainsaw_Man"
+}
+
+console.log(nombre,edad,developer,birthday,libroFavorito)
