d8dd44297b524a6f89c739386d06bb49f953d4af
Luissicart <109523221+Luissicart@users.noreply.github.com>
2022-10-31 13:07:03 +0100

    Add files via upload

---
+++ Prueba-1er-proyecto/deberesMath.js
@@ -0,0 +1,12 @@
+
+const altura_cm = 183
+const altura_m = 1.83
+const peso_kg = 90.3
+
+const altura_red = Math.ceil(altura_m)
+console.log(altura_red)
+const peso_red = Math.floor(peso_kg)
+console.log(peso_red)
+
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log(sonIguales)
